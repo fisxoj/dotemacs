854d1782df2ad2cef0998d6b57fb7add448880d8
Matt Novenstern <mnovenstern@codecademy.com>
2022-09-15 10:33:47 -0400

    add fast org link copy function

--- init.el
+++ init.el
@@ -12,6 +12,15 @@
 (setq custom-file (locate-user-emacs-file "custom.el"))
 
 (use-package org
-  :ensure org-plus-contrib)
+  :ensure org-plus-contrib
+  :bind-keymap (("C-c C-w" . f/fast-copy))
+  :config
+  ;; https://emacs.stackexchange.com/questions/63037/fast-way-to-copy-a-link-at-point-in-org-mode
+  (defun f/link-fast-copy ()
+    (interactive)
+    (let* ((context (org-element-context))
+           (type (org-element-type context)))
+      (when (eq type 'link)
+        (kill-new (org-element-property :raw-link context))))))
 
 (org-babel-load-file (locate-user-emacs-file "Matt.org"))
