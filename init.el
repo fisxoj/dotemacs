dfa0f5e27a778f9af7adb4b39dc9ad8e3c96593b
Matt Novenstern <mnovenstern@codecademy.com>
2025-11-12 12:00:40 -0500

    init: stop installing org from a special place

--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
 (package-initialize)
 
